7a48a5f8066f8cc8e3811d695f858d9b00436195
Nikita Kazak <kitkazak@gmail.com>
2022-04-21 00:30:41 +0300

    REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,13 +1,15 @@
 const req = require("express/lib/request");
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel');
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'Get goals'
-    });
+const goals = await Goal.find();
+
+    res.status(200).json(goals);
 });
 
 // @desc    Set goal
@@ -16,29 +18,49 @@ const getGoals = asyncHandler(async (req, res) => {
 const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400);
-        throw new Error('Pls add a text field');
+        throw new Error('Please add a text field');
     }
 
-    res.status(200).json({
-        message: 'Set goal'
+    const goal = await Goal.create({
+       text: req.body.text 
     });
+
+    res.status(200).json(goal);
 });
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: `Update goal ${ req.params.id }`
-    });
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { 
+        new: true 
+    }); 
+
+    res.status(200).json(updatedGoal);
 });
 
 // @desc    Get goals
 // @route   GET /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const deletedGoal = await Goal.findByIdAndDelete(req.params.id);
+
     res.status(200).json({
-        message: `Delete goal ${ req.params.id }`
+        id: req.params.id
     });
 });
 
