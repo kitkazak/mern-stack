5722ee1615abdf5dc278773834fa139e658d2ccb
Nikita Kazak <kitkazak@gmail.com>
2022-05-07 19:28:28 +0300

    redux auth slice

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,36 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+
+// Get user from localStorage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        // ERROR: no-unused-expressions
+        // reset: (state) => {
+        //     state.isLoading = false,
+        //     state.isSuccess = false,
+        //     state.isError = false,
+        //     state.message = ''
+        // }
+        reset: function (state) {
+            state.isLoading = false;
+            state.isSuccess = false;
+            state.isError = false;
+            state.message = '';  
+        }
+    },
+    extraReducers: () => {}
+})
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
